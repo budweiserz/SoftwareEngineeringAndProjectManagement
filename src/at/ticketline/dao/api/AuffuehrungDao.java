c41dde002ab62d41d605f00e58defab5161ebaee
Michael Ion <me@michaelion.net>
2013-05-10 14:11:40 +0200

    add search of Auffuehrung #20884

--- src/at/ticketline/dao/api/AuffuehrungDao.java
+++ src/at/ticketline/dao/api/AuffuehrungDao.java
@@ -1,9 +1,15 @@
 package at.ticketline.dao.api;
 
+import java.util.List;
+
 import at.ticketline.dao.GenericDao;
 import at.ticketline.entity.Auffuehrung;
 
 
 public interface AuffuehrungDao extends GenericDao<Auffuehrung,Integer> {
 
+    /**
+     * Gibt eine Liste der Auffuehrungen zurück, die den Werten von query entsprechen
+     */
+    public List<Auffuehrung> findByAuffuehrung(Auffuehrung query);
 }
--- src/at/ticketline/dao/jpa/AuffuehrungDaoJpa.java
+++ src/at/ticketline/dao/jpa/AuffuehrungDaoJpa.java
@@ -1,10 +1,65 @@
 package at.ticketline.dao.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import at.ticketline.dao.GenericDaoJpa;
 import at.ticketline.dao.api.AuffuehrungDao;
 import at.ticketline.entity.Auffuehrung;
+import at.ticketline.entity.Saal;
+import at.ticketline.entity.Veranstaltung;
 
 
 public class AuffuehrungDaoJpa extends GenericDaoJpa<Auffuehrung,Integer> implements AuffuehrungDao {
+
+    /**
+     * @author Michael Ion
+     */
+    @Override
+    public List<Auffuehrung> findByAuffuehrung(final Auffuehrung auffuehrung) {
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<Auffuehrung> query = builder.createQuery(Auffuehrung.class);
+        Root<Auffuehrung> rootAuffuehrung = query.from(Auffuehrung.class);
+        
+        List<Predicate> wherePredicates = new ArrayList<Predicate>();
+        
+        /*
+         *  Suchkriterien sind: Datum/Uhrzeit, Preis, Veranstaltung, Säle
+         */
+        if(auffuehrung.getDatumuhrzeit() != null) {
+            
+        }
+        
+        if(auffuehrung.getVeranstaltung() != null && auffuehrung.getVeranstaltung().getBezeichnung() != null) {
+            String vb = auffuehrung.getVeranstaltung().getBezeichnung().replace('*', '%').replace('?', '_').toUpperCase();
+            wherePredicates.add( builder.like( builder.upper( rootAuffuehrung.<Veranstaltung>get("veranstaltung").<String>get("nachname") ), vb) );
+        }
+        
+        if(auffuehrung.getSaal() != null && auffuehrung.getSaal().getBezeichnung() != null) {
+            String sb = auffuehrung.getSaal().getBezeichnung().replace('*', '%').replace('?', '_').toUpperCase();
+            wherePredicates.add( builder.like( builder.upper( rootAuffuehrung.<Saal>get("saal").<String>get("nachname") ), sb) );
+        }
+        
+        if(auffuehrung.getPreis() != null) {
+            
+        }
+        
+        Predicate whereClause = builder.and(wherePredicates.toArray(new Predicate[0]));
+        
+        query.where(whereClause);
+        
+        return this.entityManager.createQuery(query).getResultList();
+    }
+    
+    
+    
+    
+    
+    
 
 }

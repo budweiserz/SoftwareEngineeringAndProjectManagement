dd8586cc7f383a45932566a148191784c18a358d
Rafael Konlechner <r.konlechner@gmail.com>
2013-05-15 15:04:16 +0200

    Add TestDataGenerator, begin to fill db #21281

---
+++ test/at/ticketline/test/DataGenerator.java
@@ -0,0 +1,10 @@
+package at.ticketline.test;
+
+public class DataGenerator {
+
+    public static void main(String[] args) {
+        
+        TestInitializer.init();        
+        TestUtility.generateTestData();
+    }
+}
--- test/at/ticketline/test/TestUtility.java
+++ test/at/ticketline/test/TestUtility.java
@@ -7,18 +7,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.metamodel.EntityType;
 
+import at.ticketline.dao.DaoFactory;
 import at.ticketline.dao.EntityManagerUtil;
+import at.ticketline.dao.api.EngagementDao;
+import at.ticketline.dao.api.KuenstlerDao;
+import at.ticketline.entity.Engagement;
 import at.ticketline.entity.Kuenstler;
 
 public class TestUtility {
 
     public static void resetDatabase() {
 
-        EntityManager entityManager = EntityManagerUtil
-                .getCurrentEntityManager();
+        EntityManager entityManager = EntityManagerUtil.getCurrentEntityManager();
 
-        Set<EntityType<?>> entities = entityManager.getMetamodel()
-                .getEntities();
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
         String query = "DELETE FROM ";
 
@@ -32,16 +34,52 @@ public class TestUtility {
 
         EntityManagerUtil.commitTransaction();
     }
-    
+
     public static void generateTestData() {
-    	
-    	resetDatabase();
-    	
-    	ArrayList<Kuenstler> kuenstler = new ArrayList<Kuenstler>();
-    	
-    	for (int i = 0; i < 20; ) {
-    		
-    		
-    	}
+
+        resetDatabase();
+
+        KuenstlerDao kuenstlerDao = (KuenstlerDao) DaoFactory.getByEntity(Kuenstler.class);
+        EngagementDao engagementDao = (EngagementDao) DaoFactory.getByEntity(Engagement.class);
+
+        Kuenstler k;
+        Engagement e;
+
+        ArrayList<Kuenstler> kuenstler = new ArrayList<Kuenstler>();
+        ArrayList<Engagement> engagements = new ArrayList<Engagement>();
+
+        // Kuenstler
+        for (int i = 0; i < 20; i++) {
+
+            k = EntityGenerator.getValidKuenstler(i);
+            kuenstler.add(k);
+            kuenstlerDao.persist(k);
+        }
+
+        // Engagements
+        for (int i = 0; i < 200; i++) {
+
+            e = EntityGenerator.getValidEngagement(i);
+            engagements.add(e);
+            engagementDao.persist(e);
+
+            kuenstler.get(i % 20).getEngagements().add(e);
+            e.setKuenstler(kuenstler.get(i % 20));
+        }
+
+        /*
+         * setting foreign relationships
+         */
+        EntityManagerUtil.beginTransaction();
+
+        for (int i = 0; i < 200; i++) {
+
+            engagementDao.persist(engagements.get(i));
+            if (i < 20) {
+                kuenstlerDao.persist(kuenstler.get(i));
+            }
+        }
+
+        EntityManagerUtil.commitTransaction();
     }
 }

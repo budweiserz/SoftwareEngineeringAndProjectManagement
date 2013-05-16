191fe3648694946c8907221a61decfb3accdd7ab
Florian Klampfer <f.klampfer@gmail.com>
2013-05-16 14:48:36 +0200

    implement base new tab handler

--- src/at/ticketline/kassa/handlers/OpenVeranstaltungsSucheHandler.java
+++ src/at/ticketline/kassa/handlers/OpenVeranstaltungsSucheHandler.java
@@ -1,59 +1,7 @@
-
 package at.ticketline.kassa.handlers;
 
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@SuppressWarnings("restriction")
-public class OpenVeranstaltungsSucheHandler {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(OpenKuenstlerHandler.class);
-
-	
-	@Execute
-	public void execute(
-			MApplication application,
-			EModelService modelService,
-			EPartService partService,
-			IEclipseContext context
-			) throws ExecutionException {
-		openPart(application, modelService, partService, context);
-	}
-
-	private void openPart(MApplication application,
-			EModelService modelService,
-			EPartService partService,
-			IEclipseContext context
-			) throws ExecutionException {
-
-		// Öffene PartStack (von Applikation Model)
-		MPartStack stack = (MPartStack) modelService.find("at.ticketline.kassa.partstack.main", application);
-
-		// Erstelle Part (basierend auf einen PartDescriptor im Application Model)
-		MPart part = partService.createPart("at.ticketline.partdescriptor.searchVeranstaltung");
-
-		// Allgemeine Einstellungen des Parts
-		part.setLabel("Suche Veranstaltung");
-		part.setTooltip("Eine Suche für eine Veranstaltung starten");
-		part.setCloseable(true);
-
-		stack.getChildren().add(part);
-
-		// Part anzeigen
-		try {
-			partService.showPart(part, PartState.ACTIVATE);
-		} catch (Exception e) {
-			LOG.error("Part fuer neuen Veranstaltungssuche konnte nicht geoeffnet werden: {}", e.getMessage());
-		}
-	}
-
+public class OpenVeranstaltungsSucheHandler extends NewTabHandler {
+    public OpenVeranstaltungsSucheHandler() {
+        super("at.ticketline.partdescriptor.searchVeranstaltung", "Suche Veranstaltung", "Eine Suche für eine Veranstaltung starten");
+    }
 }

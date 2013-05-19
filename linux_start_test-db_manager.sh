c1ecccd565b937374911e59d9844bfc0a43ac452
Rafael Konlechner <r.konlechner@gmail.com>
2013-05-19 18:39:48 +0200

    Hotfix: Linux scripts

--- linux_start_test-db_manager.sh
+++ linux_start_test-db_manager.sh
@@ -1,1 +1,1 @@
-java -cp lib/hsqldb-2.2.9.jar org.hsqldb.util.DatabaseManagerSwing -url jdbc:hsqldb:hsql://localhost/ticketline -user sa
+java -cp lib/hsqldb-2.2.9.jar org.hsqldb.util.DatabaseManagerSwing -url jdbc:hsqldb:hsql://localhost/test -user sa

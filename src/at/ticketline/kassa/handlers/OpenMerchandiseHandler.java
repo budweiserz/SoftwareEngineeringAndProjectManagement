d079f02488743801f575634d221cf505dadc93f8
Florian Klampfer <f.klampfer@gmail.com>
2013-06-12 16:55:19 +0200

    fix double open #23252

--- src/at/ticketline/kassa/handlers/OpenMerchandiseHandler.java
+++ src/at/ticketline/kassa/handlers/OpenMerchandiseHandler.java
@@ -1,9 +1,12 @@
 package at.ticketline.kassa.handlers;
 
 public class OpenMerchandiseHandler extends NewTabHandler {
-
 	public OpenMerchandiseHandler() {
 		super("at.ticketline.partdescriptor.merchandise", "Merchandiseartikel", "Merchandiseartikel kaufen");
 	}
-
+	
+	@Override
+    protected boolean canOpenMultiple() {
+	    return false;
+	}
 }
--- src/at/ticketline/kassa/handlers/OpenTopTenVeranstaltungenHandler.java
+++ src/at/ticketline/kassa/handlers/OpenTopTenVeranstaltungenHandler.java
@@ -4,4 +4,9 @@ public class OpenTopTenVeranstaltungenHandler extends NewTabHandler {
     public OpenTopTenVeranstaltungenHandler() {
         super("at.ticketline.partdescriptor.topTenVeranstaltungen", "Top 10 Veranstaltungen", "Top 10 Veranstaltungen ansehen");
     }
+
+	@Override
+    protected boolean canOpenMultiple() {
+	    return false;
+	}
 }
